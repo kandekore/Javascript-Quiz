7f93cae04f616f0f73b5a7346ef88d3bf1ed4889
Kandekore <darren@kandekore.net>
2024-02-21 09:08:58 +0000

    server and procfile

--- server/server.js
+++ server/server.js
@@ -11,6 +11,15 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json()); 
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 const Score = require('./models/Scores');
 
 // REST endpoints

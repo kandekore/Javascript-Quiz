2d8f8b8a7fd2d403db7b190a0b22d9c649d41af2
Kandekore <darren@kandekore.net>
2024-12-25 16:56:49 +0000

    git uri to url 3

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // Load environment variables
 const { connect, connection } = require("mongoose");
 
 // Use the environment variable or provide a default for development
-const mongoDBUri = process.env.MONGODB_URI 
+const mongoDBUri = process.env.MONGODB_URI || process.env.MONGO_URL; 
 
 connect(mongoDBUri, {
   useNewUrlParser: true,
